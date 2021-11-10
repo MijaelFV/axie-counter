c8dae84af48a61a136b4ca5113e3499348b427c9
Mijael Flores Vega <68754287+MijaelFV@users.noreply.github.com>
2021-11-09 22:16:05 -0300

    created app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="w-full h-full bg-white flex justify-center items-center">
+      <div className="w-80 h-80 bg-gray-500 rounded-3xl"></div>
+    </div>
+  );
+}
+
+export default App;
