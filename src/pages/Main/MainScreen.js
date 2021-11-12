6975a16a1bd90ce57f41f92ed2ec331977bba855
Mijael Flores Vega <68754287+MijaelFV@users.noreply.github.com>
2021-11-11 22:37:05 -0300

    Add team logo & fix img positions

--- src/pages/Main/MainScreen.js
+++ src/pages/Main/MainScreen.js
@@ -6,6 +6,7 @@ import Rounds from './components/Rounds';
 import WoodTexture from "../../assets/wood.jpg";
 import Logo from "../../assets/logo.png";
 import Github from "../../assets/github.png"
+import CondorGaming from "../../assets/condorGaming.png"
 
 export function MainScreen() {
   const [energy, setEnergy] = useState(3)
@@ -13,7 +14,7 @@ export function MainScreen() {
   const [card, setCard] = useState(6)
 
   return (
-    <div className="w-full h-full flex flex-col  items-center">
+    <div className="w-full h-full flex flex-col items-center">
       <AppBar 
         style={{  
             backgroundImage: 'url(' + WoodTexture + ')',
@@ -37,13 +38,14 @@ export function MainScreen() {
           />
         </div>
       </AppBar>
-      <div className="flex flex-col sm:flex-row bg-gradient-to-tr mt-auto mb-auto py-3">
+      <div className="flex flex-col sm:flex-row bg-gradient-to-tr mt-auto mb-auto py-14">
         <Rounds setEnergy={setEnergy} round={round} setRound={setRound} setCard={setCard}/>
         <Energys energy={energy} setEnergy={setEnergy} />
         <Cards card={card} setCard={setCard} />
       </div>
-        <div className="flex items-center relative bottom-16">
-          <img src={Github} alt="Github logo." className="h-5 w-5" />
+        <img src={CondorGaming} alt="Github logo." className="h-24 mb-4" />
+        <div className="flex items-center mb-4">
+          <img src={Github} alt="Github logo." className="h-4 w-4" />
           <a href="https://github.com/MijaelFV/axie-counter" className="pl-1"> <i className="text-lg"> Github </i></a>
         </div>
     </div>
