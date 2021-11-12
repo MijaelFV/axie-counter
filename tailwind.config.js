a61bf6088c293d2bc335f56eb2497ff80ddea8de
Mijael Flores Vega <68754287+MijaelFV@users.noreply.github.com>
2021-11-11 22:50:58 -0300

    deploy gh-pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
+// tailwind.config.js
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.js',
+  ],
+  theme: {},
+  variants: {},
   plugins: [],
 }
